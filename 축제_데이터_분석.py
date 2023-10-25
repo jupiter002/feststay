1ce62fdb53bc68966f5ce0f8de83a2ffb5576bd7
jupiter002 <jik7194@naver.com>
2023-10-25 13:40:06 +0900

    축제 그래프 페이지 폰트 설정 추가

--- 축제_데이터_분석.py
+++ 축제_데이터_분석.py
@@ -4,18 +4,28 @@ import seaborn as sns
 
 import streamlit as st
 
+import matplotlib.font_manager as fm
 
-import platform
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
-if platform.system() == 'Windows':
-    rc('font', family='NanumGothic')
 
+#import platform
+#from matplotlib import font_manager, rc
+#plt.rcParams['axes.unicode_minus'] = False
+#if platform.system() == 'Windows':
+#    rc('font', family='NanumGothic')
+
+@st.cache_data
+def fontRegistered():
+    fm.fontManager.addfont('fonts/IBMPlexSansKR-Text.ttf')
+    fm._load_fontmanager(try_read_cache=False)
+    plt.rc('font', family='IBM Plex Sans KR')
 
 
 # 멀티 페이지용 제목
 st.set_page_config(page_title='FestStay',
                    page_icon='🕍', layout="wide")
+
+fontRegistered()
+
 
 st.sidebar.header('축제데이터 분석!')
 st.title('축제데이터 분석 페이지예요!')
@@ -28,23 +38,29 @@ fes_name_count=fes.groupby('시도명')['방문객수합계'].sum().sort_values(
 
 fes_budget=fes.groupby('시도명')['예산합계'].sum().sort_values(ascending=False)
 
-fes_budget
 
 colors = sns.color_palette('hls', len(fes_budget))
-fig, ax = plt.subplots(figsize=(16, 12))
+fig, ax = plt.subplots(figsize=(10, 6))
 fes_budget.plot.bar(stacked=True, color=colors, rot=0, fontsize=15, ax=ax)
 #sns.barplot(x='시도명',data=fes_name_count, errwidth=False)
-ax.set_xlabel('', fontsize=20)
-ax.set_ylabel('축제 지역별 예산합계', fontsize=20)
+ax.set_xlabel('', fontsize=15)
+ax.set_ylabel('축제 지역별 예산합계', fontsize=15)
 
 ax.set_title('2022년 전국 축제 예산합계', fontsize=30, pad=30)
-ax.legend(loc='upper left', fontsize=20)
+ax.legend(loc='upper left', fontsize=15)
 ax.grid(visible=False)
 
 
 
 st.pyplot(fig)
 
+
+
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
 
 #plt.figure(figsize=(16,12))
 #plt.xticks(fontsize=15)
@@ -60,15 +76,20 @@ st.pyplot(fig)
 for i in range(len(fes_name_count)):
     fes_name_count[i] = fes_name_count[i] / 100
 
+
+
+
+
+
 colors = sns.color_palette('hls', len(fes_name_count))
-fig, ax = plt.subplots(figsize=(16, 12))
+fig, ax = plt.subplots(figsize=(10, 6))
 fes_name_count.plot.bar(stacked=True, color=colors, rot=0, fontsize=15, ax=ax)
-#sns.barplot(x='시도명',data=fes_name_count, errwidth=False)
-ax.set_xlabel('', fontsize=20)
-ax.set_ylabel('축제 개최 수', fontsize=20)
+
+ax.set_xlabel('', fontsize=15)
+ax.set_ylabel('축제 개최 수', fontsize=15)
 
 ax.set_title('2022년 전국 축제 방문객수합계 (단위:100명)', fontsize=30, pad=30)
-ax.legend(loc='upper left', fontsize=20)
+ax.legend(loc='upper left', fontsize=15)
 ax.grid(visible=False)
 
 
@@ -103,22 +124,35 @@ df = pd.read_csv('./data/fesJN2023_월별 축제종류 개최수.csv')
 
 # Streamlit File *.py
 
-
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
 pivot_df = df.pivot(index='월', columns='축제종류', values='개최수')
 selected_categories = ['생태자연', '문화예술', '특산물', '전통역사', '기타(주민화합등)']
 
 selected_categories = st.selectbox('',['생태자연','문화예술','특산물','전통역사','기타(주민화합등)'])
-st.write(f'2023년 전라남도의 {selected_categories}축제는 이 정도 있어요!')
+st.title(f'2023년 전라남도의 {selected_categories}축제는 이 정도 있어요!')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
 
 pivot_df = pivot_df[selected_categories].copy()
 
 colors = sns.color_palette('hls', len(selected_categories))
-fig, ax = plt.subplots(figsize=(16, 12))
+fig, ax = plt.subplots(figsize=(10, 6))
 pivot_df.plot.bar(stacked=True, color=colors, rot=0, fontsize=15, ax=ax)
-ax.set_xlabel('월', fontsize=20)
-ax.set_ylabel('축제 개최 수', fontsize=20)
+ax.set_xlabel('월', fontsize=15)
+ax.set_ylabel('축제 개최 수', fontsize=15)
 ax.set_title('2023년 전라남도 월별 축제 종류', fontsize=30, pad=30)
-ax.legend(loc='upper left', fontsize=20)
+ax.legend(loc='upper left', fontsize=15)
 ax.grid(visible=False)
 
 
